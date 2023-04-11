48e4102a5dd77dcf3a86c5efd6de1be9e77bcd0d
zetlen <zetlen@gmail.com>
2023-04-11 15:16:31 -0500

    initial commit of zsh completion generators

---
+++ zsh-completion-generators.plugin.zsh
@@ -0,0 +1,19 @@
+i_have() {
+	command -v "$1" &>/dev/null
+}
+
+MY_COMPDIR="${0:A:h}/build"
+
+KNOWN_GENERATORS="${0:A:h}/generators.tsv"
+
+mkdir -p "$MY_COMPDIR"
+
+while IFS=$'\t' read -r CLI PRINT_COMPLETION; do
+  GENERATED_COMPLETION="${MY_COMPDIR}/_${CLI}";
+  if command -v "$CLI" &> /dev/null && [ -n "$GENERATED_COMPLETION" ]; then
+    eval "$PRINT_COMPLETION" > "$GENERATED_COMPLETION"
+  fi
+done < "$KNOWN_GENERATORS"
+
+fpath+="$MY_COMPDIR"
+
