69fc111edc9e0803e2b169f44d255927939dfcdb
zetlen <zetlen@gmail.com>
2023-04-11 21:20:01 -0500

    remove unnecessary dir

--- zsh-completion-generators.plugin.zsh
+++ zsh-completion-generators.plugin.zsh
@@ -1,19 +1,9 @@
-i_have() {
-	command -v "$1" &>/dev/null
-}
-
-MY_COMPDIR="${0:A:h}/build"
-
 KNOWN_GENERATORS="${0:A:h}/generators.tsv"
 
-mkdir -p "$MY_COMPDIR"
-
 while IFS=$'\t' read -r CLI PRINT_COMPLETION; do
-  GENERATED_COMPLETION="${MY_COMPDIR}/_${CLI}";
+  GENERATED_COMPLETION="_${CLI}";
   if command -v "$CLI" &> /dev/null && [ -z "$_comps[$CLI]" ] && [ -n "$GENERATED_COMPLETION" ]; then
     eval "$PRINT_COMPLETION" > "$GENERATED_COMPLETION"
   fi
 done < "$KNOWN_GENERATORS"
-
-fpath+="$MY_COMPDIR"
 
