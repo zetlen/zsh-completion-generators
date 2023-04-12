99708b9492d49d0a0d74341406f525a3bdf2ada3
zetlen <zetlen@gmail.com>
2023-04-12 17:02:42 -0500

    fix pathing

--- zsh-completion-generators.plugin.zsh
+++ zsh-completion-generators.plugin.zsh
@@ -1,7 +1,8 @@
-KNOWN_GENERATORS="${0:A:h}/generators.tsv"
+local MY_DIR="${0:A:h}"
+local KNOWN_GENERATORS="${MY_DIR}/generators.tsv"
 
 while IFS=$'\t' read -r CLI PRINT_COMPLETION; do
-  GENERATED_COMPLETION="_${CLI}";
+  local GENERATED_COMPLETION="${MY_DIR}/_${CLI}";
   if command -v "$CLI" &> /dev/null && [ -z "$_comps[$CLI]" ] && [ -n "$GENERATED_COMPLETION" ]; then
     eval "$PRINT_COMPLETION" > "$GENERATED_COMPLETION"
   fi
