671389ea11819f4bed0dab09b7280fef29c0587b
James Zetlen <jzetlen@adobe.com>
2023-05-25 13:28:47 -0500

    refactor for clearer and correct logic

--- zsh-completion-generators.plugin.zsh
+++ zsh-completion-generators.plugin.zsh
@@ -2,9 +2,18 @@ local MY_DIR="${0:A:h}"
 local KNOWN_GENERATORS="${MY_DIR}/generators.tsv"
 
 while IFS=$'\t' read -r CLI PRINT_COMPLETION; do
+
   local GENERATED_COMPLETION="${MY_DIR}/_${CLI}";
-  if command -v "$CLI" &> /dev/null && [ -z "$_comps[$CLI]" ] && [ -z "$GENERATED_COMPLETION" ]; then
-    eval "$PRINT_COMPLETION" > "$GENERATED_COMPLETION"
-  fi
+
+  # only for commands that are actually installed
+  command -v "$CLI" &> /dev/null || continue
+  #
+  # only if a completion doesn't already exist for it
+  [ -z "$_comps[$CLI]" ] || continue
+  #
+  # only if the file doesn't already exist
+  [ -f "$GENERATED_COMPLETION" ] && continue
+
+  eval "$PRINT_COMPLETION" > "$GENERATED_COMPLETION"
 done < "$KNOWN_GENERATORS"
 
