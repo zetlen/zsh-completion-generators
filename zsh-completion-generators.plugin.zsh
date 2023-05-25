1c0c125f855c4ad251bb88f0c4c940cd4ebb79e3
James Zetlen <jzetlen@adobe.com>
2023-05-25 12:38:45 -0500

    flipped logic for caching

--- zsh-completion-generators.plugin.zsh
+++ zsh-completion-generators.plugin.zsh
@@ -3,7 +3,7 @@ local KNOWN_GENERATORS="${MY_DIR}/generators.tsv"
 
 while IFS=$'\t' read -r CLI PRINT_COMPLETION; do
   local GENERATED_COMPLETION="${MY_DIR}/_${CLI}";
-  if command -v "$CLI" &> /dev/null && [ -z "$_comps[$CLI]" ] && [ -n "$GENERATED_COMPLETION" ]; then
+  if command -v "$CLI" &> /dev/null && [ -z "$_comps[$CLI]" ] && [ -z "$GENERATED_COMPLETION" ]; then
     eval "$PRINT_COMPLETION" > "$GENERATED_COMPLETION"
   fi
 done < "$KNOWN_GENERATORS"
